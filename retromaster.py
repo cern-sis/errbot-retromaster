5df718e4ece696204579bd6c18754b77ba3bc86b
karolina-siemieniuk-morawska <karolina.siemieniukmorawska@gmail.com>
2023-03-09 13:25:40 +0100

    changed stream

--- retromaster.py
+++ retromaster.py
@@ -19,7 +19,7 @@ class Retromaster(BotPlugin):
 
     def schedule_job(self):
         scheduler = BackgroundScheduler()
-        scheduler.add_job(self.pick_retromaster, 'interval', days=14, start_date='2023-03-09 13:00:00')
+        scheduler.add_job(self.pick_retromaster, 'interval', days=14, start_date='2023-03-09 13:30:00')
         scheduler.start()
 
     def get_all_subscribers_from_stream(self, bot_handler, stream):
@@ -43,7 +43,7 @@ class Retromaster(BotPlugin):
         request = dict(
             type='stream',
             to=stream,
-            subject='Retrospective',
+            subject='retrospective',
             content=self.generate_message(bot_handler, stream),
         )
 
