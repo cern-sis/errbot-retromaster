f8a9299063a3d87b3ba2e6eeac876f3c9ec8d2ff
Parth Shandilya <24358501+ParthS007@users.noreply.github.com>
2023-03-24 15:08:10 +0100

    refactor: add timer to fire bot

--- retromaster.py
+++ retromaster.py
@@ -1,54 +1,29 @@
-from errbot import BotPlugin, botcmd
-from apscheduler.schedulers.background import BackgroundScheduler
 import random
 
-class Retromaster(BotPlugin):
-    '''
-      This bot will allow users to randomly pick the host of our biweekly retrospective meetings. 
-    '''
+from errbot import BotPlugin, botcmd
+from threading import Timer
 
-    def zulip(self):
-        return self._bot.client
 
-    def scheduler(self):
-        scheduler = BackgroundScheduler()
-        return scheduler
+TWO_WEEKS_IN_SECONDS = 60 # 1 Minute
 
+class RetromasterPicker(BotPlugin):
     def activate(self):
         super().activate()
-        self.schedule_job()
-        self.scheduler().start()
+        self.start_periodic_task()
 
-    def deactivate(self):
-        super().deactivate()
-        self.schedule_job().remove()
-
-    def schedule_job(self):
-        return self.scheduler().add_job(self.pick_retromaster, 'interval', days=14)
-
-    def get_all_subscribers_from_stream(self, bot_handler, stream):
-        return bot_handler.get_subscribers(stream=stream)['subscribers']
-
-    def pick_random_user(self, bot_handler, stream):
-        return random.choice(self.get_all_subscribers_from_stream(bot_handler, stream))
-
-    def fetch_user_data(self, bot_handler, stream):
-        return bot_handler.get_user_by_id(self.pick_random_user(bot_handler, stream))['user']
-
-    def generate_message(self, bot_handler, stream):
-        name = self.fetch_user_data(bot_handler, stream)['full_name']
-        return f'Our next retro master is **{name}** 🎉. The expectations are super high!'
+    def start_periodic_task(self):
+        Timer(TWO_WEEKS_IN_SECONDS, self.pick_retromaster).start()
+    
+    def pick_retromaster(self):
+        stream_name = 'test'  # Replace with the name of the stream
+        topic = 'Retromaster'  # Replace with the topic
+        users = ['Benjamin Bergia', 'Ernesta Petraityte', 'Harris Tzovanakis', 'Karolina Siemieniuk-Morawska' , 'Marcjanna Jedrych', 'Miguel Garcia Garcia', 'Jimil Desai', 'Pamfilos F', 'Parth Shandilya']
+        retromaster = random.choice(users)
+        message = f"Our next retro master is **{retromaster}** 🎉. The expectations are super high!"
+        self.send(stream_name, topic, message)
+        self.start_periodic_task()
 
     @botcmd
-    def pick_retromaster(self, msg, args):
-        bot_handler = self.zulip()
-        stream = 'test'
-
-        request = dict(
-            type='stream',
-            to=stream,
-            subject='Retrospective',
-            content=self.generate_message(bot_handler, stream),
-        )
-
-        bot_handler.send_message(request)
+    def pick_manual_retromaster(self, msg, args):
+        """Manually pick a retromaster."""
+        self.pick_retromaster()
