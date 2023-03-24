7bdc311c764c4c31b977ce787a9e3f6baee0fc5c
Parth Shandilya <24358501+ParthS007@users.noreply.github.com>
2023-03-24 15:16:33 +0100

    gloabl: use build_identifier()

--- retromaster.py
+++ retromaster.py
@@ -4,7 +4,8 @@ from errbot import BotPlugin, botcmd
 from threading import Timer
 
 
-TWO_WEEKS_IN_SECONDS = 60 # 1 Minute
+TWO_WEEKS_IN_SECONDS = 60  # 1 Minute
+
 
 class RetromasterPicker(BotPlugin):
     def activate(self):
@@ -13,14 +14,25 @@ class RetromasterPicker(BotPlugin):
 
     def start_periodic_task(self):
         Timer(TWO_WEEKS_IN_SECONDS, self.pick_retromaster).start()
-    
+
     def pick_retromaster(self):
-        stream_name = 'test'  # Replace with the name of the stream
-        topic = 'Retromaster'  # Replace with the topic
-        users = ['Benjamin Bergia', 'Ernesta Petraityte', 'Harris Tzovanakis', 'Karolina Siemieniuk-Morawska' , 'Marcjanna Jedrych', 'Miguel Garcia Garcia', 'Jimil Desai', 'Pamfilos F', 'Parth Shandilya']
+        stream_name = "test"  # Replace with the name of the stream
+        topic = "Retromaster"  # Replace with the topic
+        users = [
+            "Benjamin Bergia",
+            "Ernesta Petraityte",
+            "Harris Tzovanakis",
+            "Karolina Siemieniuk-Morawska",
+            "Marcjanna Jedrych",
+            "Miguel Garcia Garcia",
+            "Jimil Desai",
+            "Pamfilos F",
+            "Parth Shandilya",
+        ]
         retromaster = random.choice(users)
         message = f"Our next retro master is **{retromaster}** 🎉. The expectations are super high!"
-        self.send(stream_name, topic, message)
+        destination = self.build_identifier(f"{stream_name}@{self._bot.bot_config.CHATROOM_FN}@{topic}")
+        self.send(destination, message)
         self.start_periodic_task()
 
     @botcmd
