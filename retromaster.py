90be896b76242ab7a1e78698d98dd9a7dd335cb8
Parth Shandilya <24358501+ParthS007@users.noreply.github.com>
2023-03-24 15:57:37 +0100

    global: add logging and fix the scheduler

--- retromaster.py
+++ retromaster.py
@@ -2,11 +2,16 @@ import random
 
 from errbot import BotPlugin, botcmd
 from apscheduler.schedulers.background import BackgroundScheduler
+import logging
 
 TWO_WEEKS_IN_SECONDS = 60  # 1 Minute
 
 
 class RetromasterPicker(BotPlugin):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.log = logging.getLogger(__name__)
+
     def activate(self):
         super().activate()
         self.start_periodic_task()
@@ -33,8 +38,12 @@ class RetromasterPicker(BotPlugin):
         retromaster = random.choice(users)
         message = f"Our next retro master is **{retromaster}** 🎉. The expectations are super high!"
         destination = self.build_identifier(f"#{{{{{stream_name}}}}}*{{{{{topic}}}}}")
-        self.send(destination, message)
-        self.start_periodic_task()
+        self.log.info(f"Sending message to: {destination}")
+        try:
+            self.send(destination, message)
+            self.log.info("Message sent successfully.")
+        except Exception as e:
+            self.log.error(f"Failed to send message: {e}")
 
     @botcmd
     def pick_manual_retromaster(self, msg, args):
